aacf0a39346a78651847db172c61157dd42a24f9
dewasemadi <dewasemadi3@gmail.com>
2022-01-16 09:17:36 +0800

    solved implement strStr()

---
+++ ImplementStrStr.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int strStr(String haystack, String needle) {
+        int len1 = haystack.length(), len2 = needle.length();
+        if (needle.length() == 0)
+            return 0;
+
+        for (int i = 0; i < haystack.length(); i++) {
+            if (i + len2 <= len1 && haystack.substring(i, i + len2).equals(needle))
+                return i;
+        }
+        return -1;
+    }
+}
+
+public class ImplementStrStr {
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        System.out.println(solution.strStr("hello", "ll"));
+    }
+}
